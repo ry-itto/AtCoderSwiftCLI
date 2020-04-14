849a751d3eea1250823b1dc050b64cf28b86b84e
ryoya ito <horusu.dqx@gmail.com>
2020-04-15 01:37:02 +0900

    create main command

--- Sources/AtCoderSwiftCLI/main.swift
+++ Sources/AtCoderSwiftCLI/main.swift
@@ -1,1 +1,11 @@
-print("Hello World")
+import ArgumentParser
+
+struct AtCoderSwiftCommand: ParsableCommand {
+    static var configuration: CommandConfiguration = .init(
+        commandName: "atcoder-swift",
+        abstract: "AtCoder Swift CLI",
+        subcommands: [],
+        defaultSubcommand: nil)
+}
+
+AtCoderSwiftCommand.main()
