ccdf17f0a9db24a4b48e7131bb2972a3c6e69f5e
ryoya ito <horusu.dqx@gmail.com>
2020-04-15 02:20:02 +0900

    generate project file

--- Sources/AtCoderSwiftCLI/SubCommands/Generate.swift
+++ Sources/AtCoderSwiftCLI/SubCommands/Generate.swift
@@ -1,8 +1,39 @@
 import ArgumentParser
+import Foundation
+import ProjectSpec
+import XcodeProj
+import XcodeGenKit
 
 extension AtCoderSwiftCommand {
     struct Generate: ParsableCommand {
         static var configuration: CommandConfiguration = .init(
             abstract: "Generate AtCoder Project")
+
+        @Argument()
+        var projectName: String
+
+        func run() throws {
+            let project: Project = .init(
+                name: projectName,
+                targets: [
+                    .init(name: "A", type: .commandLineTool, platform: .macOS, sources: ["A"]),
+                ]
+            )
+            let fileWriter = FileWriter(project: project)
+
+            let xcodeProject: XcodeProj
+            do {
+                let projectGenerator = ProjectGenerator(project: project)
+                xcodeProject = try projectGenerator.generateXcodeProject()
+            } catch {
+                return
+            }
+
+            do {
+                try fileWriter.writeXcodeProject(xcodeProject)
+            } catch {
+                return
+            }
+        }
     }
 }
