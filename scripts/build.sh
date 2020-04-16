8cc53ca691300a93ca9b1138747ae6cc6afbed44
ryoya ito <horusu.dqx@gmail.com>
2020-04-16 13:44:48 +0900

    remove ModuleCache

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 swift build -c release
 if [ $? -gt 0 ]; then
     echo 'Build failed. Clean .build and re-run.'
-    rm -rf .build
+    rm -rf .build/release/ModuleCache
     swift build -c release
 fi
